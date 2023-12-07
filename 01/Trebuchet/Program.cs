f6932a0666ba9032b4ff45638c7316c69dcef363
Xemrox <xemrox@users.noreply.github.com>
2023-12-07 15:04:44 +0100

    (feat) day 1 part 1 & 2 with input xemrox

--- 01/Trebuchet/Program.cs
+++ 01/Trebuchet/Program.cs
@@ -1,4 +1,6 @@
 ﻿
+using System.Linq;
+
 using Shared;
 
 var inputData = DataLoader.LoadInputData();
@@ -27,11 +29,29 @@ foreach (var inputSet in inputData)
 
 static int Part1(PuzzleInput<string> input)
 {
-    return 0;
+    return input.Content
+        .Select(x => x.Where(char.IsAsciiDigit))
+        .Select(x => new { first = x.First() - '0', last = x.Last() - '0' })
+        .Select(x => x.first * 10 + x.last)
+        .Sum();
 }
 
 static int Part2(PuzzleInput<string> input)
 {
-    return 0;
+    return input.Content
+        .Select(x => x
+            .Replace("one", "one1one")
+            .Replace("two", "two2two")
+            .Replace("three", "three3three")
+            .Replace("four", "four4four")
+            .Replace("five", "five5five")
+            .Replace("six", "six6six")
+            .Replace("seven", "seven7seven")
+            .Replace("eight", "eight8eight")
+            .Replace("nine", "nine9nine"))
+        .Select(x => x.Where(char.IsAsciiDigit).ToArray())
+        .Select(x => new { first = x.First() - '0', last = x.Last() - '0' })
+        .Select(x => x.first * 10 + x.last)
+        .Sum();
 }
 
